ba864888cc8baf78843f4cfffb2d3d33bfcee00e
Tomasz Buras <tomaszburas.dev@gmail.com>
2022-02-27 12:22:41 +0100

    Edit dotenv

--- client/dist/config.js
+++ client/dist/config.js
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 export const PORT = process.env.PORT || 3000;
 export const database = process.env.DATABASE || 'mongodb://localhost:27017/mega-war';
 export const ACCESS_TOKEN = process.env.ACCESS_TOKEN || "Random3334#String";
